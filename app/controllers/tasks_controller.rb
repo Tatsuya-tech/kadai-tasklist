6026684eb062c0db5bcee85801b0e7b7167b4423
tatsuya <programming00lesson@gmail.com>
2020-11-01 13:14:19 +0000

    migration_before

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,43 @@
 class TasksController < ApplicationController
+    
+  def index
+      @tasks = Task.all
+  end
+
+  def show
+      @task =Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = "正常に作成されました"
+      redirect_to @task
+    
+    else
+      flash.now[danger] = "作成に失敗しました"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
+
 end
