9b2049f1b313973720309b3a342d4a6369e7abfd
tatsuya <programming00lesson@gmail.com>
2020-11-01 16:21:54 +0000

    削除まで動作確認

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,13 +26,28 @@ class TasksController < ApplicationController
   end
 
   def edit
-  
+    @task =Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = "正常に更新されました"
+      redirect_to @task
+    
+    else
+      flash.now[:danger] = "更新されませんでした"
+      render :edit
+    end
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = "正常に削除されました"
+    redirect_to tasks_url
   end
   
   private
